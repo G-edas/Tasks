c595a0e9b3e1529622ff54c80b93c9fa516210d7
gdambra <gdambra@LT00084>
2023-02-08 18:02:22 +0200

    v2

--- converter.py
+++ converter.py
@@ -1,77 +1,73 @@
-import operator
+import constants
+import logging
 
-def converter(a):
-    number_dict = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
-    6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten', 11: 'eleven', 12: 'twelve',13:
-    'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen'
-    , 18: 'eighteen', 19: 'nineteen', 20: 'twenty', 30:'thirty', 40:'forty', 50: 'fifty',
-    60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety', 100: 'hundred', 1000: 'thousand', 
-    1000000: 'million', 1000000000: 'billion', 1000000000000: 'trillion'
-    }
-    sort_data = sorted(number_dict.items(), reverse=True)
-    sorted_number_dict = dict(sort_data)
-    number_to_append = []
-    number_tuple = tuple()
-    number = str(a)
-    number_list = list(number)
-
-    if(len(number) < 5):
-
-        for i in range(len(number_list)):
-            
-            number_to_append.append(number_list[i] + int(len(number_list) - i - 1) * '0')
-            number_to_append = [int(el) for el in number_to_append]
-            number_to_append_without_0 = [item for item in number_to_append if item != 0]
-        
-        if(sum(number_to_append_without_0[-2:]) in range(11,20)):
-            
-            summed_digits = sum(number_to_append_without_0[-2:])
-            del number_to_append_without_0[-2:]
-            number_to_append_without_0.append(summed_digits)
+def number_converter(n):
     
-        for i in range(len(number_to_append_without_0)):          
-            counter = list(str(number_to_append_without_0[i])).count('0')
-            if(counter == 0):
-                counter = 'none'
-            if(len(str(number_to_append_without_0[i])) - 1 == counter and counter !=1 ):
+    numbers_devided = []
+    number_tuple = tuple()
+    number = str(n)
+    number_list = list(number)
+    minus = ''
+    
+    if(number[0] == '-'):
+            minus = 'minus '
+            number = number[1:]
+
+    if(len(number) < 5 or int(number) in constants.number_dict.keys() 
+       or number == constants.ZEROS_ONE_MILLION or number == constants.ZEROS_ONE_BILLION):
+
+        if (number == constants.ZEROS_ONE_THOUSAND or number == constants.ZEROS_ONE_MILLION 
+            or number == constants.ZEROS_ONE_BILLION):
+            
+            numbers_devided = [int('1'+number)]
+            
+        else:   
+            for i in range(len(number_list)):
                 
-                number_to_append_without_0[i] = list(number_tuple + (
-                                                    int(str(number_to_append_without_0[i])[0])
-                                                    ,round(number_to_append_without_0[i]/ int(str(number_to_append_without_0[i])[0]))
-                                                    ))
+                numbers_devided.append(number_list[i] + int(len(number_list) - i - 1) * '0')
+                numbers_devided = [int(el) for el in numbers_devided]
+                numbers_devided = [item for item in numbers_devided if item != 0]
+            
+            if(sum(numbers_devided[-2:]) in constants.numberTEEN):
                 
+                summed_digits = sum(numbers_devided[-2:])
+                del numbers_devided[-2:]
+                numbers_devided.append(summed_digits)
+                
+            for i in range(len(numbers_devided)):          
+                counter = list(str(numbers_devided[i])).count('0')
+                if(counter == 0):
+                    counter = 'none'
+                if(len(str(numbers_devided[i])) - 1 == counter and counter !=1 ):
+                    
+                    numbers_devided[i] = list(number_tuple + (
+                                                        int(str(numbers_devided[i])[0])
+                                                        ,round(numbers_devided[i]/ int(str(numbers_devided[i])[0]))
+                                                        ))   
         result = []
-        for item in number_to_append_without_0:
+        for item in numbers_devided:
             if isinstance(item, list):
                 result.extend(item)
             else:   
                 result.append(item)
-        print(result)
 
         number_in_words = ''
         for list_item in result:
-            for dict_key in sorted_number_dict.keys():
+            for dict_key in constants.sorted_number_dict.keys():
                 if dict_key == list_item:
-                    number_in_words += ''+sorted_number_dict[dict_key]+' '
+                    number_in_words += ''+constants.sorted_number_dict[dict_key]+' '
                         
         return number_in_words
        
     elif(len(number) >= 5):
         
-        if(len(number) in range(5, 7)):
-            parsing_number = [int(number) // 1000, 1000,  int(number) % 1000]
-            # print(1250000 % 1000000)
-        elif(len(number) in range(7, 10)):
-            parsing_number = [int(number) // 1000000, 1000000,  int(number) % 1000000]
-        elif(len(number) in range(10,13)):
-            parsing_number = [int(number) // 1000000000, 1000000000,  int(number) % 1000000000]
+        constants.number_devidor(number)
         
-        print(parsing_number)
-        full_word = ''
-        for el in parsing_number:
-            full_word = full_word + converter(el)
-        return full_word  
-
-
-print(converter(2222))
+    full_word = ''
+    for el in constants.number_devidor(number):
+        full_word = full_word + number_converter(el)
+       
+    return minus + full_word
+    
+print(number_converter(-132481981564))
 
